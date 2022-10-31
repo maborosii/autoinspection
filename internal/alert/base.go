438f8112184ea006faca188f9b1d642d43c6d9bf
maborosii <bonvovageonepiece@gmail.com>
2022-10-31 20:18:45 +0800

    modify output columns add metrics value in now, one day ago,one week ago

--- internal/alert/base.go
+++ internal/alert/base.go
@@ -30,7 +30,7 @@ func NewBaseAlertMessage(job, instance, alertMessage, alertMetricsLimit, alertMe
 }
 
 func (n *baseAlertMessage) PrintAlert(mType string) string {
-	return fmt.Sprintf("%s 指标异常 >>> job: %s, instance: %s,  告警信息: %s, 当前值: %s, 预警值: %s, 指标值（当前）: %.2f%%, 指标值（一天前）: %.2f%%, 指标值（一周前）: %.2f%%\n", mType, n.job, n.instance, n.alertMessage, n.alertMetricsUsage, n.alertMetricsLimit, n.metricsNow, n.metricsBefore1Day, n.metricsBefore1Week)
+	return fmt.Sprintf("%s 指标异常 >>> job: %s, instance: %s, 告警信息: %s, 当前值: %s, 预警值: %s, 指标值（当前）: %s, 指标值（一天前）: %s, 指标值（一周前）: %s\n", mType, n.job, n.instance, n.alertMessage, n.alertMetricsUsage, n.alertMetricsLimit, n.metricsNow, n.metricsBefore1Day, n.metricsBefore1Week)
 }
 func (n *baseAlertMessage) PrintAlertFormatTable(mType string) table.Row {
 	return table.Row{fmt.Sprintf("%s 指标异常", mType), n.job, n.instance, "", n.alertMessage, n.alertMetricsUsage, n.alertMetricsLimit, n.metricsNow, n.metricsBefore1Day, n.metricsBefore1Week}
