c540f875b80d2fe5e4a700b8129c9594e60c9232
maborosii <bonvovage@163.com>
2022-11-08 23:33:07 +0800

    change switch-case to strategy mode

--- infra/starters/rules_starter.go
+++ infra/starters/rules_starter.go
@@ -7,6 +7,7 @@ import (
 	"node_metrics_go/infra"
 	rs "node_metrics_go/internal/rules"
 	"node_metrics_go/pkg/setting"
+	"sync"
 
 	"github.com/mitchellh/mapstructure"
 )
@@ -19,58 +20,154 @@ func (d *RulesStarter) Setup(conf *setting.Config) {
 	d.setupRules(conf)
 }
 
+// type addRulesFunc func(rs.RuleItf)
+// type addRulesMap map[string]addRulesFunc
+
+// var rulesMap = addRulesMap{}
+
+// // register handle
+// func (a addRulesMap) registerRuleType(ruleType string, f addRulesFunc) error {
+// 	if _, exists := rulesMap[ruleType]; exists {
+// 		return fmt.Errorf("%s has been registered", ruleType)
+// 	}
+// 	a[ruleType] = f
+// 	return nil
+// }
+type ruleRegister interface {
+	register([]interface{})
+}
+type nodeRulesRegister struct{}
+
+func (n nodeRulesRegister) register(confRules []interface{}) {
+	nodeRule := new(rs.NodeRule)
+	for _, jj := range confRules {
+		err := mapstructure.Decode(jj, nodeRule)
+		if err != nil {
+			panic(fmt.Sprintf("mapstructure rules for node occur error: %s", err))
+		}
+		global.NotifyRules[nodeRule.GetRuleJob()] = nodeRule
+	}
+}
+
+type redisRulesRegister struct{}
+
+func (n redisRulesRegister) register(confRules []interface{}) {
+	redisRule := new(rs.RedisRule)
+	for _, jj := range confRules {
+		err := mapstructure.Decode(jj, redisRule)
+		if err != nil {
+			panic(fmt.Sprintf("mapstructure rules for redis occur error: %s", err))
+		}
+		global.NotifyRules[redisRule.GetRuleJob()] = redisRule
+	}
+}
+
+type kafkaRulesRegister struct{}
+
+func (n kafkaRulesRegister) register(confRules []interface{}) {
+	kafkaRule := new(rs.KafkaRule)
+	for _, jj := range confRules {
+		err := mapstructure.Decode(jj, kafkaRule)
+		if err != nil {
+			panic(fmt.Sprintf("mapstructure rules for kafka occur error: %s", err))
+		}
+		global.NotifyRules[kafkaRule.GetRuleJob()] = kafkaRule
+	}
+}
+
+type rabbitMQRulesRegister struct{}
+
+func (n rabbitMQRulesRegister) register(confRules []interface{}) {
+	rabbitMQRule := new(rs.RabbitMQRule)
+	for _, jj := range confRules {
+		err := mapstructure.Decode(jj, rabbitMQRule)
+		if err != nil {
+			panic(fmt.Sprintf("mapstructure rules for rabbitmq occur error: %s", err))
+		}
+		global.NotifyRules[rabbitMQRule.GetRuleJob()] = rabbitMQRule
+	}
+}
+
+type elasticSearchRulesRegister struct{}
+
+func (n elasticSearchRulesRegister) register(confRules []interface{}) {
+	elasticSearchRule := new(rs.ElasticSearchRule)
+	for _, jj := range confRules {
+		err := mapstructure.Decode(jj, elasticSearchRule)
+		if err != nil {
+			panic(fmt.Sprintf("mapstructure rules for elasticSearch occur error: %s", err))
+		}
+		global.NotifyRules[elasticSearchRule.GetRuleJob()] = elasticSearchRule
+	}
+}
+
+var doReg sync.Once
+var registerMap = make(map[string]ruleRegister)
+
 func (d *RulesStarter) setupRules(conf *setting.Config) {
 	log.Println("init rules setting ...")
+	doReg.Do(func() {
+		registerMap["node"] = new(nodeRulesRegister)
+		registerMap["redis"] = new(redisRulesRegister)
+		registerMap["kafka"] = new(kafkaRulesRegister)
+		registerMap["rabbitmq"] = new(rabbitMQRulesRegister)
+		registerMap["es"] = new(elasticSearchRulesRegister)
+	})
 	for tt, j := range conf.Rules {
 		// parse template config
-		switch tt {
-		case "node":
-			for _, jj := range j {
-				nodeRule := new(rs.NodeRule)
-				err := mapstructure.Decode(jj, nodeRule)
-				if err != nil {
-					panic(fmt.Sprintf("mapstructure rules for node occur error: %s", err))
-				}
-				global.NotifyRules[nodeRule.GetRuleJob()] = nodeRule
-			}
-		case "redis":
-			for _, jj := range j {
-				redisRule := new(rs.RedisRule)
-				err := mapstructure.Decode(jj, redisRule)
-				if err != nil {
-					panic(fmt.Sprintf("mapstructure rules for redis occur error: %s", err))
-				}
-				global.NotifyRules[redisRule.GetRuleJob()] = redisRule
-			}
-		case "kafka":
-			for _, jj := range j {
-				kafkaRule := new(rs.KafkaRule)
-				err := mapstructure.Decode(jj, kafkaRule)
-				if err != nil {
-					panic(fmt.Sprintf("mapstructure rules for kafka occur error: %s", err))
-				}
-				global.NotifyRules[kafkaRule.GetRuleJob()] = kafkaRule
-			}
-		case "rabbitmq":
-			for _, jj := range j {
-				rabbitMQRule := new(rs.RabbitMQRule)
-				err := mapstructure.Decode(jj, rabbitMQRule)
-				if err != nil {
-					panic(fmt.Sprintf("mapstructure rules for rabbitMQ occur error: %s", err))
-				}
-				global.NotifyRules[rabbitMQRule.GetRuleJob()] = rabbitMQRule
-			}
-		case "es":
-			for _, jj := range j {
-				elasticSearchRule := new(rs.ElasticSearchRule)
-				err := mapstructure.Decode(jj, elasticSearchRule)
-				if err != nil {
-					panic(fmt.Sprintf("mapstructure rules for elasticSearch occur error: %s", err))
-				}
-				global.NotifyRules[elasticSearchRule.GetRuleJob()] = elasticSearchRule
-			}
-		default:
+		if _, ok := registerMap[tt]; !ok {
 			panic(fmt.Sprintf("not suitable rule type in config, rule.type: %s", tt))
 		}
+		registerMap[tt].register(j)
+
+		// switch tt {
+		// case "node":
+		// 	for _, jj := range j {
+		// 		nodeRule := new(rs.NodeRule)
+		// 		err := mapstructure.Decode(jj, nodeRule)
+		// 		if err != nil {
+		// 			panic(fmt.Sprintf("mapstructure rules for node occur error: %s", err))
+		// 		}
+		// 		global.NotifyRules[nodeRule.GetRuleJob()] = nodeRule
+		// 	}
+		// case "redis":
+		// 	for _, jj := range j {
+		// 		redisRule := new(rs.RedisRule)
+		// 		err := mapstructure.Decode(jj, redisRule)
+		// 		if err != nil {
+		// 			panic(fmt.Sprintf("mapstructure rules for redis occur error: %s", err))
+		// 		}
+		// 		global.NotifyRules[redisRule.GetRuleJob()] = redisRule
+		// 	}
+		// case "kafka":
+		// 	for _, jj := range j {
+		// 		kafkaRule := new(rs.KafkaRule)
+		// 		err := mapstructure.Decode(jj, kafkaRule)
+		// 		if err != nil {
+		// 			panic(fmt.Sprintf("mapstructure rules for kafka occur error: %s", err))
+		// 		}
+		// 		global.NotifyRules[kafkaRule.GetRuleJob()] = kafkaRule
+		// 	}
+		// case "rabbitmq":
+		// 	for _, jj := range j {
+		// 		rabbitMQRule := new(rs.RabbitMQRule)
+		// 		err := mapstructure.Decode(jj, rabbitMQRule)
+		// 		if err != nil {
+		// 			panic(fmt.Sprintf("mapstructure rules for rabbitMQ occur error: %s", err))
+		// 		}
+		// 		global.NotifyRules[rabbitMQRule.GetRuleJob()] = rabbitMQRule
+		// 	}
+		// case "es":
+		// 	for _, jj := range j {
+		// 		elasticSearchRule := new(rs.ElasticSearchRule)
+		// 		err := mapstructure.Decode(jj, elasticSearchRule)
+		// 		if err != nil {
+		// 			panic(fmt.Sprintf("mapstructure rules for elasticSearch occur error: %s", err))
+		// 		}
+		// 		global.NotifyRules[elasticSearchRule.GetRuleJob()] = elasticSearchRule
+		// 	}
+		// default:
+		// 	panic(fmt.Sprintf("not suitable rule type in config, rule.type: %s", tt))
+		// }
 	}
 }
